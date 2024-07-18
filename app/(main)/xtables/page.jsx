08915b364c9ba5021df989c85707265eb1ac71a6
kobel <kobelei335@gmail.com>
2024-07-18 16:42:05 -0700

    buggies

--- app/(main)/xtables/page.jsx
+++ app/(main)/xtables/page.jsx
@@ -48,7 +48,7 @@ const Dashboard = () => {
         return () => {
             TerminalService.off('command', commandHandler);
         };
-    }, []);
+    }, [commandHandler]);
     useEffect(() => {
         setData(convertJSON(rawJSON));
     }, [rawJSON]);
