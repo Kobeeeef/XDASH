4581a4f3b4db3f1128f8ef4cd873b7c177ea1eb6
kobel <kobelei335@gmail.com>
2024-07-19 16:57:40 -0700

    fixed up sum stuff

--- app/(main)/xtables/stats/page.jsx
+++ app/(main)/xtables/stats/page.jsx
@@ -6,18 +6,19 @@ import { WebsocketContext } from '../../../../layout/context/websocketcontext';
 import TimeAgo from '../../../../components/TimeAgo';
 const Dashboard = () => {
     const { isConnected, lastConnectionUpdate, sendMessageAndWaitForCondition } = useContext(WebsocketContext);
-    const [statusData, setStatusData] = useState({});
+    const [infoData, setInfoData] = useState({});
     const [lastStatusUpdate, setLastStatusUpdate] = useState(new Date());
     const [lastClientsUpdate, setLastClientsUpdate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             if (isConnected) {
-                sendMessageAndWaitForCondition({ type: 'XTABLES-STATUS' }, (m) => m.type === 'XTABLES-STATUS').then((message) => {
-                    setStatusData((a) => {
+                sendMessageAndWaitForCondition({ type: 'XTABLES-STATISTICS' }, (m) => m.type === 'XTABLES-STATISTICS').then((message) => {
+                    setInfoData((a) => {
+                        message.message.info = JSON.parse(message.message.info)
                         if (message.message.connected !== a?.connected) setLastStatusUpdate(new Date());
-                        if (message.message.clients !== a?.clients) setLastClientsUpdate(new Date());
-
+                        if (message.message.info.totalClients !== a?.info?.totalClients) setLastClientsUpdate(new Date());
+                        console.log(message.message)
                         return message.message;
                     });
                 }).catch(() => {
@@ -25,7 +26,7 @@ const Dashboard = () => {
 
 
             }
-        }, 100);
+        }, 500);
 
         // Cleanup interval on component unmount
         return () => clearInterval(intervalId);
@@ -56,7 +57,7 @@ const Dashboard = () => {
                         <div>
                             <span className="block text-500 font-medium mb-3">XTABLES Status</span>
                             <div
-                                className={"text-900 font-medium text-xl " + (isConnected ? statusData?.connected ? 'text-white' : 'text-red-600' : 'text-red-600')}>{isConnected ? statusData?.connected ? 'Connected' : 'Disconnected' : 'Disconnected'}</div>
+                                className={"text-900 font-medium text-xl " + (isConnected ? infoData?.connected ? 'text-white' : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? 'Connected' : 'Disconnected' : 'Disconnected'}</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
                              style={{ width: '2.5rem', height: '2.5rem' }}>
@@ -73,7 +74,7 @@ const Dashboard = () => {
                         <div>
                             <span className="block text-500 font-medium mb-3">Clients</span>
                             <div
-                                className={"text-900 font-medium text-xl " + (isConnected ? statusData?.connected ? statusData?.clients ? 'text-white' : "text-red-600" : 'text-red-600' : 'text-red-600')}>{isConnected ? statusData?.connected ? statusData?.clients || 'Unknown' : 'Disconnected' : 'Disconnected'}</div>
+                                className={"text-900 font-medium text-xl " + (isConnected ? infoData?.connected ? infoData?.info?.totalClients ? 'text-white' : "text-red-600" : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? infoData?.info?.totalClients || 'Unknown' : 'Disconnected' : 'Disconnected'}</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
                              style={{ width: '2.5rem', height: '2.5rem' }}>
@@ -89,7 +90,7 @@ const Dashboard = () => {
                     <div>
                         <span className="block text-500 font-medium mb-3">Health</span>
                         <div
-                            className={"text-900 font-medium text-xl "  + (isConnected ? statusData?.connected ? statusData?.clients ? 'text-white' : "text-red-600" : 'text-red-600' : 'text-red-600')}>{isConnected ? statusData?.connected ? statusData?.clients ? 'GOOD' : "BAD" : 'Disconnected' : 'Disconnected'}</div>
+                            className={"text-900 font-medium text-xl "  + (isConnected ? infoData?.connected ? infoData?.info?.health === "GOOD" ? 'text-green-600' : "text-red-600" : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? infoData?.info?.health || "UNKNOWN" : 'Disconnected' : 'Disconnected'}</div>
                     </div>
                     <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
                          style={{ width: '2.5rem', height: '2.5rem' }}>
@@ -99,7 +100,70 @@ const Dashboard = () => {
                 <TimeAgo date={lastStatusUpdate} />
             </div>
         </div>
-
+        <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card mb-0">
+                <div className="flex justify-content-between mb-3">
+                    <div>
+                        <span className="block text-500 font-medium mb-3">IP Address</span>
+                        <div
+                            className={"text-900 font-medium text-xl "  + (isConnected ? infoData?.connected ? infoData?.info?.ip : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? infoData?.info?.ip || "UNKNOWN" : 'Disconnected' : 'Disconnected'}</div>
+                    </div>
+                    <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
+                         style={{ width: '2.5rem', height: '2.5rem' }}>
+                        <i className="pi pi-map-marker text-cyan-500 text-xl" />
+                    </div>
+                </div>
+                <TimeAgo date={lastStatusUpdate} />
+            </div>
+        </div>
+        <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card mb-0">
+                <div className="flex justify-content-between mb-3">
+                    <div>
+                        <span className="block text-500 font-medium mb-3">Round Trip Latency</span>
+                        <div
+                            className={"text-900 font-medium text-xl "  + (isConnected ? infoData?.connected ? infoData?.roundTripLatencyMS : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? infoData?.roundTripLatencyMS || "UNKNOWN" : 'Disconnected' : 'Disconnected'}</div>
+                    </div>
+                    <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
+                         style={{ width: '2.5rem', height: '2.5rem' }}>
+                        <i className="pi pi-bolt text-cyan-500 text-xl" />
+                    </div>
+                </div>
+                <TimeAgo date={lastStatusUpdate} />
+            </div>
+        </div>
+        <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card mb-0">
+                <div className="flex justify-content-between mb-3">
+                    <div>
+                        <span className="block text-500 font-medium mb-3">Network Latency</span>
+                        <div
+                            className={"text-900 font-medium text-xl "  + (isConnected ? infoData?.connected ? infoData?.networkLatencyMS : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? infoData?.networkLatencyMS || "UNKNOWN" : 'Disconnected' : 'Disconnected'}</div>
+                    </div>
+                    <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
+                         style={{ width: '2.5rem', height: '2.5rem' }}>
+                        <i className="pi pi-wifi text-cyan-500 text-xl" />
+                    </div>
+                </div>
+                <TimeAgo date={lastStatusUpdate} />
+            </div>
+        </div>
+        <div className="col-12 lg:col-6 xl:col-3">
+            <div className="card mb-0">
+                <div className="flex justify-content-between mb-3">
+                    <div>
+                        <span className="block text-500 font-medium mb-3">Total Messages</span>
+                        <div
+                            className={"text-900 font-medium text-xl "  + (isConnected ? infoData?.connected ? infoData?.info?.totalMessages : 'text-red-600' : 'text-red-600')}>{isConnected ? infoData?.connected ? infoData?.info?.totalMessages || "UNKNOWN" : 'Disconnected' : 'Disconnected'}</div>
+                    </div>
+                    <div className="flex align-items-center justify-content-center bg-blue-100 border-round"
+                         style={{ width: '2.5rem', height: '2.5rem' }}>
+                        <i className="pi pi-wifi text-cyan-500 text-xl" />
+                    </div>
+                </div>
+                <TimeAgo date={lastStatusUpdate} />
+            </div>
+        </div>
         </div>);
 };
 
