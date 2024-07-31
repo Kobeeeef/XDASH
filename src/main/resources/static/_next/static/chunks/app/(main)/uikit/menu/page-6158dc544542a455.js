3d6912b1a8c48284dd2b205546e3efa1aa57fe66
kobel <kobelei335@gmail.com>
2024-07-31 14:28:39 -0700

    device services

---
+++ src/main/resources/static/_next/static/chunks/app/(main)/uikit/menu/page-6158dc544542a455.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[429],{3356:function(n,e,u){Promise.resolve().then(u.bind(u,7645))}},function(n){n.O(0,[434,6878,6789,8285,3640,5538,7645,9253,5769,1744],function(){return n(n.s=3356)}),_N_E=n.O()}]);
